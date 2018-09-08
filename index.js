ca838962bba00a7686c73ed3f58a80df19cba01e
Tim Britton <ziberath@gmail.com>
2018-09-08 14:46:25 -0500

    landing tweaks

--- index.js
+++ index.js
@@ -4,13 +4,38 @@ import { Link, Route, Switch, Redirect, location } from "@hyperapp/router"
 import {Landing} from './landing'
 import * as firebase from 'firebase'
 const state = {
-    location: location.state
+    location: location.state,
+    user: location.user
   }
   
   const actions = {
-    location: location.actions
+    location: location.actions,
+    login: state => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+
+            actions.setUser(user)
+            
+            // ...
+          }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+          });
+    },
+    setUser: value => state => {{ user: value }}
+
   }
-  const LandingView =  Landing
+  const LandingView =  Landing(state)
   const AuthedView = () => <h2>Authed</h2>
 
 
--- landing.jsx
+++ landing.jsx
@@ -1,12 +1,14 @@
 import { h } from 'hyperapp'
 import { Link, Route, Switch, Redirect, location } from "@hyperapp/router"
 
-export const Landing = (state) => {
+export const Landing = (state, actions) => {
 
     return(
         <div>
             <h2>landing</h2>
+            <button onClick={() => actions.login()}>Google</button>
         </div>
     );
 }
 
+// export const GoogleLoginButton = (state, actions) => 
